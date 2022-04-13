33a16773b2224f2440ebaefb4c6d1e5ba3aeb62c
usmanh25 <usmanh25@yahoo.com>
2022-04-12 19:18:57 -0700

    more excess code deleted

--- frontend/reducers/track_playbar_reducer.js
+++ frontend/reducers/track_playbar_reducer.js
@@ -35,39 +35,3 @@ const trackPlaybarReducer = (state = _nullPlay, action) => {
 }
 
 export default trackPlaybarReducer
-
-
-
-
-
-
-// import {
-//     REMOVE_PLAY_TRACK,
-//     RECEIVE_PLAY_TRACK,
-//     PLAY_TRACK,
-//     PAUSE_TRACK
-// } from '../actions/track_playbar_actions';
-
-// const trackPlaybarReducer = (state={ currentTrack: null, isPlaying: false }, action) => {
-//     Object.freeze(state);
-//     const nextState = Object.assign({}, state);
-
-//     switch (action.type) {
-//         case RECEIVE_PLAY_TRACK:
-//             nextState["currentTrack"] = action.track;
-//             nextState["isPlaying"] = true;
-//             return nextState;
-//         case PLAY_TRACK:
-//             nextState["isPlaying"] = true;
-//             return nextState;
-//         case PAUSE_TRACK:
-//             nextState["isPlaying"] = false;
-//             return nextState;
-//         case REMOVE_PLAY_TRACK:
-//             return { currentTrack: null, isPlaying: false }
-//         default:
-//             return state;
-//     }
-// };
-
-// export default trackPlaybarReducer;
