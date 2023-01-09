001762ab9a016309e0c235564b931ba64bf5648a
usmanh25 <usmanh25@yahoo.com>
2023-01-09 15:53:19 -0800

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-
-  namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show] do
-      resources :tracks, only: [:index]
-    end
-    resource :session, only: [:create, :destroy]
-    resources :tracks, only: [:index, :show, :create, :update, :destroy]
-    resources :comments, only: [:create, :destroy, :index]
-  end
-    root to:"static_pages#root"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
