8700de774476d881936bdd4d69bc026de350e920
Usman Hameed <usmanh25@yahoo.com>
2025-07-25 03:14:31 -0700

    .babelrc file added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',  // adjust this if your entry is different
+  entry: './src/noiseCloud.jsx',  // adjust this if your entry is different
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
