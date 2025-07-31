7c4c7bd9cda9591e871fd53eb978469ad97be38e
Usman Hameed <usmanh25@yahoo.com>
2025-07-31 03:18:50 -0700

    fix cors url mismatch

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://songcloud-v3.vercel.app', /https:\/\/songcloud-v3-[a-z0-9]+\.vercel\.app/
+    origins 'http://localhost:3000', 'https://songcloud-v3.vercel.app'
 
     resource '*',
       headers: :any,
+      expose: ['Authorization'],  
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
