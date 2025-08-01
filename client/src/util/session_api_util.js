ae015de456c0fd29cdf9243f4fef54db039eab45
Usman Hameed <usmanh25@yahoo.com>
2025-08-01 05:51:36 -0700

    added session_store.rb

--- client/src/util/session_api_util.js
+++ client/src/util/session_api_util.js
@@ -1,28 +1,3 @@
-// const API_BASE_URL = process.env.REACT_APP_API_URL;
-
-// export const signup = user => (
-//     $.ajax({
-//       method: 'POST',
-//       url: `${API_BASE_URL}/api/users`,
-//       data: { user }
-//     })
-// );
-
-// export const login = user => (
-//     $.ajax({
-//         method: 'POST',
-//         url: `${API_BASE_URL}/api/session`,
-//         data: { user }
-//     })
-// );
-
-// export const logout = () => (
-//     $.ajax({
-//         method: 'DELETE',
-//         url: `${API_BASE_URL}/api/session`
-//     })
-// );
-
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const signup = user => (
--- client/src/util/user_api_util.js
+++ client/src/util/user_api_util.js
@@ -1,30 +1,3 @@
-// const API_BASE_URL = process.env.REACT_APP_API_URL;
-
-// export const fetchUser = (userId) => (
-//     $.ajax({
-//         method: 'GET',
-//         url: `${API_BASE_URL}/api/users/${userId}`
-//     })
-// );
-
-// export const fetchUsers = () => (
-//     $.ajax({
-//         method: 'GET',
-//         url: `${API_BASE_URL}/api/users/`
-//     })
-// );
-
-// export const updateUser = (user, userId) => (
-//     $.ajax({
-//         method: 'PATCH',
-//         url: `${API_BASE_URL}/api/users/${userId}`,
-//         data: user,
-//         contentType: false,
-//         processData: false
-//     })
-// );
-
-
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchUser = (userId) => (
