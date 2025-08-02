0aba2deb34efcfca96f2d4f793eaacd8ee669193
Usman Hameed <usmanh25@yahoo.com>
2025-08-02 06:43:54 -0700

    routes.rb vercel edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,9 @@
-# Rails.application.routes.draw do
-#   namespace :api, defaults: {format: :json} do
-#     resources :users, only: [:create, :show] do
-#       resources :tracks, only: [:index]
-#     end
-#     resource :session, only: [:create, :destroy]
-#     resources :tracks, only: [:index, :show, :create, :update, :destroy]
-#     resources :comments, only: [:create, :destroy, :index]
-#   end
-
-#   root to:"static_pages#root"
-
-# end
-# 
-#Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show] do
       resources :tracks, only: [:index]
     end
 
-    # ✅ Added :show to allow GET /api/session
     resource :session, only: [:create, :destroy, :show]
 
     resources :tracks, only: [:index, :show, :create, :update, :destroy]
@@ -27,9 +11,4 @@
   end
 
   root to: "static_pages#root"
-
-  # if Rails.env.production?
-  #   get '/migrate_db', to: 'utils#migrate'
-  #   get '/seed_db', to: 'utils#seed'
-  # end
 end
