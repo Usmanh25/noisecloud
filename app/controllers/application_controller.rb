42690c04030c3419fd67f30236d899ba327e0330
Usman Hameed <usmanh25@yahoo.com>
2025-08-03 12:27:04 -0700

    application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,28 @@
 class ApplicationController < ActionController::Base
-  # Skip CSRF only for JSON requests (like from your React frontend)
+  # Only skip CSRF for JSON (API) requests
   skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
 
   helper_method :current_user, :logged_in?
 
   def current_user
+    return @current_user if defined?(@current_user)
+
     token = session[:session_token]
-    @current_user ||= User.find_by(session_token: token)
+    if token.present?
+      @current_user = User.find_by(session_token: token)
+    else
+      @current_user = nil
+    end
+  end
+
+  def logged_in?
+    !!current_user
   end
 
   def require_logged_in
-    redirect_to new_session_url unless logged_in?
+    unless logged_in?
+      render json: { errors: ["Not logged in"] }, status: :unauthorized
+    end
   end
 
   def login!(user)
@@ -22,9 +34,5 @@ class ApplicationController < ActionController::Base
     current_user&.reset_session_token!
     session[:session_token] = nil
     @current_user = nil
-  end
-
-  def logged_in?
-    !!current_user
   end
 end
