ad2b153442c83f8af86422fc2d14d36c4889f4b8
user <user@users-MacBook-Pro.local>
2025-08-10 16:58:47 -0700

    controller

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 Rails.application.config.session_store :cookie_store,
   key: '_songcloud_session',
+  domain: Rails.env.production? ? :all : nil, # <--- explicitly allow all subdomains / cross site in prod
   same_site: Rails.env.production? ? :none : :lax,
   secure: Rails.env.production?
