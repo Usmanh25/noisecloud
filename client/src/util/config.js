a7e671bbdd4b0be834a316b63ddcb3f837e461e8
user <user@users-MacBook-Pro.local>
2025-08-10 19:49:43 -0700

    edit cors

--- client/src/util/config.js
+++ client/src/util/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '';
 export default API_BASE_URL;
