45c94c66eea1b671bba79de14f68769dc56ee5a4
user <user@users-MacBook-Pro.local>
2025-08-10 18:21:16 -0700

    sessionstore.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,11 @@
+# Rails.application.config.session_store :cookie_store,
+#   key: '_songcloud_session',
+#   domain: Rails.env.production? ? 'songcloud-e382.onrender.com' : nil,
+#   same_site: Rails.env.production? ? :none : :lax,
+#   secure: Rails.env.production?
+
 Rails.application.config.session_store :cookie_store,
   key: '_songcloud_session',
-  domain: Rails.env.production? ? 'songcloud-e382.onrender.com' : nil,
-  same_site: Rails.env.production? ? :none : :lax,
-  secure: Rails.env.production?
+  domain: 'songcloud-e382.onrender.com',  # Explicitly your backend domain
+  same_site: :none,
+  secure: true
