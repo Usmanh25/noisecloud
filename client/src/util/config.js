0f74422031f7c2aa910491e504d2f06e4ef19da8
user <user@users-MacBook-Pro.local>
2025-08-10 19:32:23 -0700

    proxy

--- client/src/util/config.js
+++ client/src/util/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || '';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000';
 export default API_BASE_URL;
